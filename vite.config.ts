f5eaa972e58f9a05f709bfbc7b5076fc27f38a57
coleflem <cole@triad.tech>
2026-06-03 20:57:21 -0500

    add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/WesPortfolio/" : "/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+    tailwindcss(),
+  ],
 }));
